0637fb56fa7b4378323b9f582654a8324b62dbb2
Dustin Fast <dustin.fast@outlook.com>
2020-03-31 19:06:19 -0500

    Added motion to eeg test -DF

--- test_eeg.py
+++ test_eeg.py
@@ -7,6 +7,7 @@ import os
 import platform
 import time
 import ctypes
+from time import sleep
 
 import atexit
 from select import select
@@ -43,6 +44,10 @@ IEE_EmoStateCreate = libEDK.IEE_EmoStateCreate
 IEE_EmoStateCreate.restype = c_void_p
 eState = IEE_EmoStateCreate()
 
+IEE_MotionDataCreate = libEDK.IEE_MotionDataCreate
+IEE_MotionDataCreate.restype = c_void_p
+hMotionData = IEE_MotionDataCreate()
+
 userID = c_uint(0)
 user = pointer(userID)
 ready = 0
@@ -57,6 +62,9 @@ maxBatteryLevelP = pointer(maxBatteryLevel)
 systemUpTime = c_float(0.0)
 wirelessStrength = c_int(0)
 
+datarate = c_uint(0)
+secs = c_float(1)
+motionChannelList = [i for i in range(11)]
 
 # -------------------------------------------------------------------------
 print "Connecting to device..."
@@ -65,7 +73,10 @@ if libEDK.IEE_EngineConnect("Emotiv Systems-5") != 0:
     print "Emotiv Engine start up failed."
     exit()
 
+print "Buffer size in secs: %f \n" % secs.value
 print("Waiting for information state update from device...")
+
+libEDK.IEE_MotionDataSetBufferSizeInSec(secs)
 
 while True:    
     state = libEDK.IEE_EngineGetNextEvent(eEvent)
@@ -86,12 +97,39 @@ while True:
                                 
                 libEDK.IS_GetBatteryChargeLevel(eState, batteryLevelP, maxBatteryLevelP)
                 
-                print('Up Time: ', systemUpTime, "\n")
-                print('Wireless Strength: ', wirelessStrength, "\n")
+                print('Up Time: ' + str(systemUpTime) + "\n")
+                print('Wireless Strength: ' + str(wirelessStrength) + "\n")
                 print('Battery: ', batteryLevel.value, "\n")
                 print('\n')
-                break
-                             
+
+                # Print the next 10 gyro events
+                print("COUNTER, GYROX, GYROY, GYROZ, ACCX, ACCY, ACCZ, MAGX, MAGY, MAGZ, TIMESTAMP")
+                
+                while True:
+                    state = libEDK.IEE_EngineGetNextEvent(eEvent)
+    
+                    if state == 0:
+                        eventType = libEDK.IEE_EmoEngineEventGetType(eEvent)
+                        libEDK.IEE_EmoEngineEventGetUserId(eEvent, user)
+
+                        libEDK.IEE_MotionDataUpdateHandle(userID, hMotionData)
+                        nSamplesTaken = c_uint(0)
+                        nSamplesTakenP = pointer(nSamplesTaken)
+                        
+                        libEDK.IEE_MotionDataGetNumberOfSample(hMotionData, nSamplesTakenP)
+                        
+                        if nSamplesTaken.value > 0:
+                            i += 1
+                            dataType = c_double * nSamplesTaken.value
+                            data = dataType()
+
+                            for sampleIdx in range(nSamplesTaken.value):
+                                sample = []
+                                for i in motionChannelList:
+                                    libEDK.IEE_MotionDataGet(hMotionData, i, data, nSamplesTaken.value)
+                                    sample.append(data[sampleIdx])
+                                print('\t'.join([str(s) for s in sample]))
+
     elif state != 0x0600:
         print "Internal error in Emotiv Engine ! "
 
