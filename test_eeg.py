51c733a647f035effb38901ee90673748759f0ad
Dustin Fast <dustin.fast@outlook.com>
2020-04-06 00:58:06 -0500

    Simplified eeg demo -DF

--- test_eeg.py
+++ test_eeg.py
@@ -1,48 +1,46 @@
 #! /usr/bin/env python
 
-import argparse
 import time
 import numpy as np
+import pandas as pd
 
 from brainflow.board_shim import BoardShim, BrainFlowInputParams
 from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    # use docs to check which parameters are required for specific board, e.g. for Cyton - set serial port
-    parser.add_argument('--timeout', type=int, help='timeout for device discovery or connection', default=0)
-    parser.add_argument('--serial-port', type=str, help='serial port', default='/dev/ttyUSB0')
-    parser.add_argument('--other-info', type=str, help='other info', default='')
-    parser.add_argument('--streamer-params', type=str, help='other info', default='')
-    parser.add_argument('--board-id', type=int, help='board id', default=0)
-    parser.add_argument('--log', action='store_true')
-    args = parser.parse_args()
-
-    params = BrainFlowInputParams()
-    params.serial_port = args.serial_port
-    params.other_info = args.other_info
-    params.timeout = args.timeout
-
-    if(args.log):
-        BoardShim.enable_dev_board_logger()
-    else:
-        BoardShim.disable_board_logger()
-
-    board = BoardShim(args.board_id, params)
-    board.prepare_session()
-
-    # board.start_stream() # use this for default options
-    board.start_stream(45000, args.streamer_params)
-    time.sleep(2)
-    # data = board.get_current_board_data(256) # get latest 256 packages or less, doesnt remove them from internal buffer
-    data = board.get_board_data() # get all data and remove it from internal buffer
-    board.stop_stream()
-    board.release_session()
-
-    print(data)
-    print(data.shape)
+CYTON_BOARD_ID = 0
+CYTON_SERIAL_PORT = '/dev/ttyUSB0'
+DATA_GATHER_TIME = 3  # seconds
 
 
 if __name__ == "__main__":
-    main()
+    # Init the board
+    params = BrainFlowInputParams()
+    params.serial_port = CYTON_SERIAL_PORT
+    BoardShim.disable_board_logger()
+    board = BoardShim(CYTON_BOARD_ID, params)
+    board.prepare_session()
+
+    sample_rate = BoardShim.get_sampling_rate(CYTON_BOARD_ID)
+    eeg_channels = BoardShim.get_eeg_channels(CYTON_BOARD_ID)
+    tstamp_channel = BoardShim.get_timestamp_channel(CYTON_BOARD_ID)
+
+    # Start data stream
+    print(f'Gathering {DATA_GATHER_TIME} seconds worth of data w/ ')
+    print(f'sample rate = {BoardShim.get_sampling_rate(CYTON_BOARD_ID)}')
+    board.start_stream()  # Default ring buffer sz = 45000
+    time.sleep(DATA_GATHER_TIME)
+
+    # Collect data from stream and close it
+    data = board.get_board_data()
+    board.stop_stream()
+    board.release_session()
+
+    # Strip data down to timestamp and eeg channels only
+    print(f'Shape = {data.shape}') 
+    data = data[[tstamp_channel] + eeg_channels, :]
+
+    # Write to stdout
+    print('Received data:')
+    print(data)
+    print(f'Shape = {data.shape}')  # shape = [num_channels x num_data_points]
