343c711dc0b0fa95a9be3ef434ec384d071e2edd
Dustin Fast <dustin.fast@outlook.com>
2020-06-10 01:35:04 -0500

    Added argprase -DF

--- aeye_typer.py
+++ aeye_typer.py
@@ -4,25 +4,33 @@
 
 __author__ = 'Dustin Fast <dustin.fast@outlook.com>'
 
+import argparse
 import pyximport; pyximport.install()
 
-# from lib.py.eyetracker_gaze import EyeTrackerGaze
+from lib.py.eyetracker_gaze import EyeTrackerGaze
 from lib.py.hud import HUD
 
 
 if __name__ == "__main__":
-    # TODO: Cmd line options for...
-    #   data collection toggle
-    #   Run eye-tracker calibration
+    # Setup cmd line args
+    parser = argparse.ArgumentParser()
+    arg_flags = ('-g', '--gaze_off')
+    arg_help_str = 'Run with gaze event logging turned off.'
+    parser.add_argument(*arg_flags,
+                        action='store_true',
+                        default=False,
+                        help=arg_help_str)
+    args = parser.parse_args()
 
-    # e = EyeTrackerGaze()
+    if not args.gaze_off:
+        e = EyeTrackerGaze()
+        e.open()
+        e.start()
+
     k = HUD()
-    
-    # e.open()
-    # e.start()
-
     k.start()
 
-    # e.stop()
-    # e.close()
+    if not args.gaze_off:
+        e.stop()
+        e.close()
 
