9ff07a558b1f458ae458a344c5140f8e0c5dfe36
Dustin Fast <dustin.fast@outlook.com>
2020-07-07 05:08:47 -0500

    Added feat explorations -DF

--- lib/py/hud_learn.py
+++ lib/py/hud_learn.py
@@ -30,12 +30,83 @@ del _conf
 
 # Training data attributes
 DATA_SESSION_NAME = '2020-06-13'
+RAND_SEED = 1234
+DATA_GROUP_KEY = 'y_key_id'                  
+
+# mae_x = 8.3777
+# mae_y = 7.6306
 DATA_COL_NAMES = ['X_eyepos_left_x', 'X_eyepos_left_y', 'X_eyepos_left_z',
                   'X_eyepos_right_x', 'X_eyepos_right_y', 'X_eyepos_right_z',
                   'X_gaze_x', 'X_gaze_y', 'y_gaze_x', 'y_gaze_y', 'y_key_id']
-DATA_GROUP_KEY = 'y_key_id'                  
 DATA_COL_IDXS = [3, 4, 5, 6, 7, 8, 31, 32, 33, 34, 35]
-RAND_SEED = 1234
+
+# mae_x = 7.3667
+# mae_y = 6.7142
+# DATA_COL_NAMES = ['X_pupil_dia_l', 'X_pupil_dia_r',
+#                   'X_eyepos_left_x', 'X_eyepos_left_y', 'X_eyepos_left_z',
+#                   'X_eyepos_right_x', 'X_eyepos_right_y', 'X_eyepos_right_z',
+#                   'X_gaze_x', 'X_gaze_y', 'y_gaze_x', 'y_gaze_y', 'y_key_id']
+# DATA_COL_IDXS = [1, 2, 3, 4, 5, 6, 7, 8, 31, 32, 33, 34, 35]
+
+# mae_x = 7.9200
+# mae_y = 7.0659
+# DATA_COL_NAMES = [
+#     'X_left_pupildiameter_mm',
+#     'X_right_pupildiameter_mm',
+
+#     'X_left_eyeposition_normed_x',
+#     'X_left_eyeposition_normed_y',
+#     'X_left_eyeposition_normed_z',
+#     'X_right_eyeposition_normed_x',
+#     'X_right_eyeposition_normed_y',
+#     'X_right_eyeposition_normed_z',
+
+#     'X_left_eyecenter_mm_x',
+#     'X_left_eyecenter_mm_y',
+#     'X_left_eyecenter_mm_z',
+#     'X_right_eyecenter_mm_x',
+#     'X_right_eyecenter_mm_y',
+#     'X_right_eyecenter_mm_z',
+
+#     'X_left_gazeorigin_mm_x',
+#     'X_left_gazeorigin_mm_y',
+#     'X_left_gazeorigin_mm_z',
+#     'X_right_gazeorigin_mm_x',
+#     'X_right_gazeorigin_mm_y',
+#     'X_right_gazeorigin_mm_z',
+
+#     'X_left_gazepoint_mm_x',
+#     'X_left_gazepoint_mm_y',
+#     'X_left_gazepoint_mm_z',
+#     'X_right_gazepoint_mm_x',
+#     'X_right_gazepoint_mm_y',
+#     'X_right_gazepoint_mm_z',
+
+#     # mae_x = 7.9042
+#     # mae_y = 7.0615
+#     'X_left_gazepoint_normed_x',
+#     'X_left_gazepoint_normed_y',
+#     'X_right_gazepoint_normed_x',
+#     'X_right_gazepoint_normed_y',
+
+#     # mae_x = 7.9176
+#     # mae_y = 7.0760
+#     'X_combined_gazepoint_x',
+#     'X_combined_gazepoint_y',
+
+#     'y_gaze_x',
+#     'y_gaze_y',
+#     'y_key_id'
+# ]
+# DATA_COL_IDXS = [1, 2,
+#                  3, 4, 5, 6, 7, 8,
+#                  9, 10, 11, 12, 13, 14,
+#                  15, 16, 17, 18, 19, 20,
+#                  21, 22, 23, 24, 25, 26,
+#                  27, 28, 29, 30,
+#                  31, 32,
+#                  33, 34, 35
+# ]
 
 
 class HUDLearn(object):
@@ -147,7 +218,7 @@ class HUDTrain(HUDLearn):
 
         return df
     
-    def _train_gaze_acc(self, n_limit=25, split=0.7):
+    def _train_gaze_acc(self, n_limit=35, split=0.75):
         """ Gaze accuracy training handler.
         """
         print('Training...')
@@ -188,10 +259,9 @@ class HUDTrain(HUDLearn):
         if len(df.index) % n_limit != 0:
             raise Exception('Unexpected post-processed data shape enountered.')
 
-         # Plot and save corr matrix, for testing purposes
+        # Plot and save corr matrix, for testing purposes
         # corr = df.iloc[:, :-1].corr()
         # fig = plt.figure(figsize = (15,15))
-
         # sb.heatmap(corr, vmax=0.8, square=True)
         # plt.savefig(f'test_SVR_cor.png')
 
@@ -209,19 +279,17 @@ class HUDTrain(HUDLearn):
         scaler = MinMaxScaler()
         scaler.fit(X_train)
         X_train = scaler.transform(X_train)
-
         X_test = scaler.transform(X_test)
         
-        # Break labels into their x/y coord components.
+        # Break labels into their x/y coord components
         y_train_x_coord, y_train_y_coord = (
             y_train[:, 0].squeeze(), y_train[:, 1].squeeze())
         y_test_x_coord, y_test_y_coord = (
             y_test[:, 0].squeeze(), y_test[:, 1].squeeze())
 
-        # NOTE: Best was x = 0.9912, y = 0.9631 w C=1000 and e=6
-        model_x = SVR(kernel='rbf', C=1000, epsilon=6).fit(
+        model_x = SVR(kernel='rbf', C=1000, epsilon=.1).fit(
             X_train, y_train_x_coord)
-        model_y = SVR(kernel='rbf', C=1000, epsilon=6).fit(
+        model_y = SVR(kernel='rbf', C=1000, epsilon=.1).fit(
             X_train, y_train_y_coord)
 
         # Validate
@@ -232,7 +300,7 @@ class HUDTrain(HUDLearn):
         model_x_score = mean_absolute_error(y_test_x_coord, y_x_coord_hat)
         model_y_score = mean_absolute_error(y_test_y_coord, y_y_coord_hat)
         
-        print('Done:\n\tScore_x = %.4f\n\tScore_y = %.4f' % 
+        print('Done:\n\tmae_x = %.4f\n\tmae_y = %.4f' % 
             (model_x_score, model_y_score))
 
         # Set scaler as a model member, so it's saved with it
@@ -246,17 +314,17 @@ class HUDTrain(HUDLearn):
             pickle.dump(model_y, f)
 
         # # Plot x/y coord actual vs x/y coord pred, for testing convenience
-        # plt.figure()
-        # plt.scatter(
-        #     y_test_x_coord, y_test_y_coord, c="green", label="x/y", marker=".")
-        # plt.scatter(
-        #     y_x_coord_hat, y_y_coord_hat, c="red", marker=".",
-        #         label='x/y pred (score=%.4f|%.4f)' % (
-        #             model_x_score, model_y_score))
-        # plt.xlim([1500, 3840])
-        # plt.ylim([2160, 1500])
-        # plt.xlabel("gaze_x")
-        # plt.ylabel("gaze_y")
-        # plt.title("Perf")
-        # plt.legend()
-        # plt.savefig(f'test_SVR_acc.png')
+        plt.figure()
+        plt.scatter(
+            y_test_x_coord, y_test_y_coord, c="green", label="x/y", marker=".")
+        plt.scatter(
+            y_x_coord_hat, y_y_coord_hat, c="red", marker=".",
+                label='x/y pred (score=%.4f|%.4f)' % (
+                    model_x_score, model_y_score))
+        plt.xlim([1500, 3840])
+        plt.ylim([2160, 1500])
+        plt.xlabel("gaze_x")
+        plt.ylabel("gaze_y")
+        plt.title("Perf")
+        plt.legend()
+        plt.savefig(f'test_SVR_acc.png')
