55162e50a7a04c739a5a30a67fb5334e246de51f
Dustin Fast <dustin.fast@outlook.com>
2020-12-23 17:30:31 -0600

    Removed wav options-DF

--- lib/py/speech2txt.py
+++ lib/py/speech2txt.py
@@ -144,9 +144,6 @@ def main(ARGS):
     print('Initializing model...')
     logging.info("ARGS.model: %s", ARGS.model)
     model = deepspeech.Model(ARGS.model)
-    if ARGS.scorer:
-        logging.info("ARGS.scorer: %s", ARGS.scorer)
-        model.enableExternalScorer(ARGS.scorer)
 
     # Start audio with VAD
     vad_audio = VADAudio(aggressiveness=ARGS.vad_aggressiveness,
@@ -156,11 +153,8 @@ def main(ARGS):
     frames = vad_audio.vad_collector()
 
     # Stream from microphone to DeepSpeech using VAD
-    spinner = None
-    if not ARGS.nospinner:
-        spinner = Halo(spinner='line')
+    spinner = Halo(spinner='line')
     stream_context = model.createStream()
-    wav_data = bytearray()
     for frame in frames:
         if frame is not None:
             if spinner: spinner.start()
@@ -185,20 +179,10 @@ if __name__ == '__main__':
 
     parser.add_argument('-v', '--vad_aggressiveness', type=int, default=0,
                         help="Set aggressiveness of VAD: an integer between 0 and 3, 0 being the least aggressive about filtering out non-speech, 3 the most aggressive. Default: 3")
-    parser.add_argument('--nospinner', action='store_true',
-                        help="Disable spinner")
-    parser.add_argument('-w', '--savewav',
-                        help="Save .wav files of utterences to given directory")
-    parser.add_argument('-f', '--file',
-                        help="Read from .wav file instead of microphone")
-
     parser.add_argument('-m', '--model', required=True,
                         help="Path to the model (protocol buffer binary file, or entire directory containing all standard-named files for model)")
-    parser.add_argument('-s', '--scorer',
-                        help="Path to the external scorer file.")
     parser.add_argument('-d', '--device', type=int, default=None,
                         help="Device input index (Int) as listed by pyaudio.PyAudio.get_device_info_by_index(). If not provided, falls back to PyAudio.get_default_device().")
     
     ARGS = parser.parse_args()
-    if ARGS.savewav: os.makedirs(ARGS.savewav, exist_ok=True)
     main(ARGS)
